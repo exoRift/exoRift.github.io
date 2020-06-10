b2dee4a6bfd44d0e133b0c5b9b2cabfd6c28cc95
Arthur Neuman <arthurbneuman@gmail.com>
2020-06-10 11:28:58 -0400

    Foundation

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Router from './Router.jsx'
+
+import './styles/bootstrap.min.css'
+import './styles/index.css'
+
+ReactDOM.render(ReactDOM.render(<Router/>, document.getElementById('root')))
---
+++ src/util/routes.js
@@ -0,0 +1,12 @@
+import Home from '../Home.jsx'
+
+const routes = [
+  {
+    path: '/',
+    exact: true,
+    name: 'Home',
+    component: Home
+  }
+]
+
+export default routes
