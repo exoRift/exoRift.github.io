661ddbe9aa06774dc72b7d44eaf9b11424a6e210
Site Deployer <arthurbneuman@gmail.com>
2020-06-11 02:06:30 +0000

    Update Site for version '1.0.0'

---
+++ precache-manifest.5792da2bffa13f2bf90a699be51bc884.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb97bb79d1936e07a932cb89421691d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "025af83a48438718bcab",
+    "url": "/static/css/main.3c68fb93.chunk.css"
+  },
+  {
+    "revision": "38201607efcbbce4c566",
+    "url": "/static/js/2.e9b18c76.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.e9b18c76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "025af83a48438718bcab",
+    "url": "/static/js/main.58769a43.chunk.js"
+  },
+  {
+    "revision": "d1e1f21d5ea246d846e0",
+    "url": "/static/js/runtime-main.883917f5.js"
+  },
+  {
+    "revision": "7d8fcf8feb0ea8d433ac673c8e32d654",
+    "url": "/static/media/Splash.7d8fcf8f.png"
+  },
+  {
+    "revision": "98f4761f5931ca180aeeece3d5b50144",
+    "url": "/static/media/about.98f4761f.md"
+  }
+]);
